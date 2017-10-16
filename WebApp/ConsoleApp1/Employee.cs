1d9543861b1c09d7cd3153d933900756b4a155ba
Petr Anikyev <panikev@gmail.com>
2017-10-16 21:19:17 +0800

    4

---
+++ WebApp/ConsoleApp1/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string JobTitle { get; set; }
+        public string Department  {get; set; }
+        public DateTime EmploymentDateFrom { get; set; }
+    }
+}
---
+++ WebApp/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection(@"Persist Security Info = True; data source = localhost\SQLEXPRESS;User= anikev;Password = 1; initial catalog = LearnDb");
+            con.Open();
+    
+
+
+
+
+
+
+
+
+
+            //const string providerName = "System.Data.SqlClient";
+            //const string serverName = @"localhost\SQLEXPRESS;";
+            //string databasePach
+        }
+    }  
+}
